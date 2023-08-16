413a862e2000ffe5ef8210ef885fbd15f0a7f2ce
Alex Kaye <alexrichardkaye123@gmail.com>
2023-08-16 17:37:47 +0100

    Updated Figure S1 data

--- Figures/FigureS1/FigureS1Script.m
+++ Figures/FigureS1/FigureS1Script.m
@@ -13,7 +13,8 @@ set(0,'DefaultAxesColorOrder',brewermap(NaN,'Set2'))
 
 %note that the full CESM dataset is too large to include on GitHub, so we
 %upload the data from 1850 to 2100 from Miami for 100 different projections
-%instead.
+%instead. We don't upload the whole BEST dataset and only the areas of
+%iinterest for the same reason.
 
 
 addpath('MiamiTempDataCESM')
@@ -46,9 +47,6 @@ end
 
 TavgMiami=mean(TMatMiami,1);
  
-%Tdata=readtable('MiamiTempDataHistorical.csv'); %loads historical data
-%TdataMiami=table2array(Tdata(:,4));
-
 %% 
 
 
@@ -116,54 +114,11 @@ TavgParis=mean(TMatParis,1);
 
 
 
-
-
 %% 
 
-ncfile='../../Data/ClimateData/BESTData/Land_and_Ocean_LatLong1.nc';
-
-temp=ncread(ncfile,'temperature');
-climate=ncread(ncfile,'climatology');
-
-%% 
-
-Miamitemp=temp(100,116,:);
-Miamitemp=squeeze(Miamitemp);
-Miamitemp(2077:2079)=[];
-Miamitemp=mean(reshape(Miamitemp,12,[]),1);
-
-Miamiclimate=climate(100,116,:);
-Miamiclimate=squeeze(Miamiclimate);
-climateavg=mean(Miamiclimate);
-Miamitemp=Miamitemp+climateavg;
-
-
-%% 
-
-LAtemp=temp(63,124,:);
-LAtemp=squeeze(LAtemp);
-LAtemp(2077:2079)=[];
-LAtemp=mean(reshape(LAtemp,12,[]),1);
-
-LAclimate=climate(63,124,:);
-LAclimate=squeeze(LAclimate);
-climateavg=mean(LAclimate);
-LAtemp=LAtemp+climateavg;
-
-%% 
-
-
-Paristemp=temp(180,136,:);
-Paristemp=squeeze(Paristemp);
-Paristemp(2077:2079)=[];
-Paristemp=mean(reshape(Paristemp,12,[]),1);
-
-Parisclimate=climate(180,136,:);
-Parisclimate=squeeze(Parisclimate);
-climateavg=mean(Parisclimate);
-Paristemp=Paristemp+climateavg;
-
-
+load('../../Data/ClimateData/BESTData/BESTMiamiTempData.mat')
+load('../../Data/ClimateData/BESTData/BESTLosAngelesTempData.mat')
+load('../../Data/ClimateData/BESTData/BESTParisTempData.mat')
 
 %% Plot the figure
 
@@ -177,30 +132,29 @@ plot(t,(TMatMiami)','color',[128 128 128]/256,'LineWidth',0.5,'HandleVisibility'
 alpha(0.5)
 plot(t,TavgMiami,'color',[0 0 0])
 xlabel('Year')
-ylabel('Annual temperature anomaly ($^{\circ}$C)')
 plot(t2,Miamitemp,'color','r')
-text(1923,26.5,'Miami, Florida','FontSize',16)
+text(1923,26.5,'Miami, USA','FontSize',16)
 
 plot(t,(TMatLA)','color',[128 128 128]/256,'LineWidth',0.5,'HandleVisibility','off')
 alpha(0.5)
 plot(t,TavgLA,'color',[0 0 0])
 xlabel('Year')
-ylabel('Annual temperature anomaly ($^{\circ}$C)')
 plot(t2,LAtemp,'color','r')
-text(1923,19.5,'Los Angeles, California','FontSize',16)
+text(1923,19.5,'Los Angeles, USA','FontSize',16)
 
 plot(t,(TMatParis)','color',[128 128 128]/256,'LineWidth',0.5,'HandleVisibility','off')
 alpha(0.5)
 plot(t,TavgParis,'color',[0 0 0])
 xlabel('Year')
-ylabel('Mean annual temperature ($^{\circ}$C)')
+ylabel('Temperature ($^{\circ}$C)')
 plot(t2,Paristemp,'color','r')
 text(1923,14.8,'Paris, France','FontSize',16)
 
 xlim([1920,2100])
 ylim([10,32])
+xticks([1920,1950,1980,2010,2040,2070,2100])
 box on
-legend('CESM projections','CESM mean','BEST data','Location','NorthWest')
+legend('CESM projections','CESM mean','Real-world data','Location','NorthWest')
 
 set(gcf, 'Units', 'centimeters', 'Position', [0 0 20 15], 'PaperUnits', 'centimeters', 'PaperSize', [15 20]);
 export_fig FigureS1.pdf -pdf -r300 -Opengl 
